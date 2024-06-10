086da87594fbefaaecf742fe0bcfa7054cefa7d8
DanielCok17 <cokydano@gmail.com>
2024-06-10 22:36:17 +0200

    Added dockerization

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -34,7 +34,10 @@ switch ($command) {
     }
     "mongo" {
         mongo up
-        }
+    }
+    "docker" {
+        docker build -t danielcok17/ambulance-wl-webapi:local-build -f ${ProjectRoot}/build/docker/Dockerfile .
+    }
     default {
         throw "Unknown command: $command"
     }
