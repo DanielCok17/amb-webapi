03e6afc93fc06090a6ba6d0a0ac5ddd4dce06857
DanielCok17 <cokydano@gmail.com>
2024-06-12 16:52:41 +0200

    test

--- deployments/kustomize/install/params/init-db.js
+++ deployments/kustomize/install/params/init-db.js
@@ -1,45 +1,51 @@
-const mongoHost = process.env.AMBULANCE_API_MONGODB_HOST;
-const mongoPort = process.env.AMBULANCE_API_MONGODB_PORT;
-const database = process.env.AMBULANCE_API_MONGODB_DATABASE;
-const collection = process.env.AMBULANCE_API_MONGODB_COLLECTION;
+const mongoHost = process.env.AMBULANCE_API_MONGODB_HOST
+const mongoPort = process.env.AMBULANCE_API_MONGODB_PORT
+
+const mongoUser = ''
+const mongoPassword = ''
+
+const database = process.env.AMBULANCE_API_MONGODB_DATABASE
+const collection = process.env.AMBULANCE_API_MONGODB_COLLECTION
+
 const retrySeconds = parseInt(process.env.RETRY_CONNECTION_SECONDS || "5") || 5;
 
-console.log('mongoHost: ' + mongoHost);
-console.log('mongoPort: ' + mongoPort);
+console.log('mongoUser' + mongoUser)
+console.log('mongoPassword' + mongoPassword)
 
 // try to connect to mongoDB until it is not available
 let connection;
 while(true) {
     try {
-        connection = Mongo(`mongodb://${mongoHost}:${mongoPort}`);
+        // connection = Mongo(`mongodb://${mongoHost}:${mongoPort}`);
+        connection = Mongo(`mongodb://localhost:27017`);
         break;
     } catch (exception) {
         print(`Cannot connect to mongoDB: ${exception}`);
-        print(`Will retry after ${retrySeconds} seconds`);
+        print(`Will retry after ${retrySeconds} seconds`)
         sleep(retrySeconds * 1000);
     }
 }
 
 // if database and collection exists, exit with success - already initialized
-const databases = connection.getDBNames();
+const databases = connection.getDBNames()
 if (databases.includes(database)) {
-    const dbInstance = connection.getDB(database);
-    const collections = dbInstance.getCollectionNames();
+    const dbInstance = connection.getDB(database)
+    collections = dbInstance.getCollectionNames()
     if (collections.includes(collection)) {
-       print(`Collection '${collection}' already exists in database '${database}'`);
+       print(`Collection '${collection}' already exists in database '${database}'`)
         process.exit(0);
     }
 }
 
 // initialize
 // create database and collection
-const db = connection.getDB(database);
-db.createCollection(collection);
+const db = connection.getDB(database)
+db.createCollection(collection)
 
 // create indexes
-db[collection].createIndex({ "id": 1 });
+db[collection].createIndex({ "id": 1 })
 
-// insert sample data
+//insert sample data
 let result = db[collection].insertMany([
     {
         "id": "bobulova",
@@ -53,8 +59,8 @@ let result = db[collection].insertMany([
 ]);
 
 if (result.writeError) {
-    console.error(result);
-    print(`Error when writing the data: ${result.errmsg}`);
+    console.error(result)
+    print(`Error when writing the data: ${result.errmsg}`)
 }
 
 // exit with success
